97ba196cfa045f618f74913302f90f99925bb276
S00187348 <S00187348@LAB-B1042-33A>
2020-01-23 16:44:08 +0000

    created method band and band list

--- labsheet1/MainWindow.xaml.cs
+++ labsheet1/MainWindow.xaml.cs
@@ -20,12 +20,30 @@ namespace labsheet1
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<band> all_bands = new List<band>();
+
         public MainWindow()
         {
            
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            band b1 = new band("Beatles", "John, Paul, George, Ringo", 1960);
+            band b2 = new band("Beatles", "John, Paul, George, Ringo", 1960);
+            band b3 = new band("Beatles", "John, Paul, George, Ringo", 1960);
+            band b4 = new band("Beatles", "John, Paul, George, Ringo", 1960);
+            band b5 = new band("Beatles", "John, Paul, George, Ringo", 1960);
+            band b6 = new band("Beatles", "John, Paul, George, Ringo", 1960);
 
+            all_bands.Add(b1);
+            all_bands.Add(b2);
+            all_bands.Add(b3);
+            all_bands.Add(b4);
+            all_bands.Add(b5);
+            all_bands.Add(b6);
 
+            lbxBands.ItemsSource = all_bands;
+        }
     }
 }
--- labsheet1/band.cs
+++ labsheet1/band.cs
@@ -6,9 +6,28 @@ using System.Threading.Tasks;
 
 namespace labsheet1
 {
-    class band
+    class band : IComparable<band>
     {
         public string Name { get; set; }
+        public int Year { get; set; }
+        public string Members{ get; set; }
 
+        public band(string name, string members, int year)
+        {
+             Name = name;
+            Members = members;
+            Year =year;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+
+        public int CompareTo(band other)
+        {
+            return this.Name.CompareTo(other.Name);
+        }
     }
 }
